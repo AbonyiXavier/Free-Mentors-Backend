6ffc04e498e7a3dcca977726d58ef6c36c19df53
AbonyiXavier <francisabonyi@gmail.com>
2020-04-02 15:36:19 +0100

    changed mysql query to get all mentors

--- src/controllers/mentor.js
+++ src/controllers/mentor.js
@@ -3,7 +3,9 @@ import dbConnection from "../database/db";
 export default class mentorController {
   static async getAllMentor(req, res) {
     try {
-      const getAllMentor = `SELECT * FROM user WHERE role = "mentor" `;
+      const getAllMentor = `SELECT mentorId, firstName, lastName, email , password, 
+      address, bio, occupation, expertise, isAdmin, role
+      FROM mentor, user WHERE mentor.userId = user.userId `;
       let results = await dbConnection.query(getAllMentor);
       //   console.log(results);
       return res.status(200).json({
